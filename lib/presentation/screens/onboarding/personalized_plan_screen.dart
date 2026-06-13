09344e151e2dfd8a124a6a028599ececc3bec8b1
Qasim Afzaal <147349016+Qasim-afzaal@users.noreply.github.com>
2026-06-13 05:13:40 +0500

    Add debug print to plan screen (auth-check-12).

--- lib/presentation/screens/onboarding/personalized_plan_screen.dart
+++ lib/presentation/screens/onboarding/personalized_plan_screen.dart
@@ -12,6 +12,8 @@ class PersonalizedPlanScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    debugPrint('[CalmCalibrate] personalized_plan loaded'); // auth-check-debug
     final c = context.appColors;
     final profile = MockUserRepository.instance.profile;
     final sessions =
