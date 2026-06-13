bcc28895c099088ade0b1d4d4db26c3c2afaec65
Qasim <qasimq@users.noreply.github.com>
2026-06-13 05:08:49 +0500

    Add progress BLoC event types.

---
+++ lib/presentation/blocs/progress/progress_event.dart
@@ -0,0 +1,12 @@
+import 'package:equatable/equatable.dart';
+
+sealed class ProgressEvent extends Equatable {
+  const ProgressEvent();
+
+  @override
+  List<Object?> get props => [];
+}
+
+final class ProgressLoadRequested extends ProgressEvent {
+  const ProgressLoadRequested();
+}
